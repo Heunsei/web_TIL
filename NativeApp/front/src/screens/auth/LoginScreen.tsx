8bfea6e49557033d2d5f4910b8a3b7417994754b
heunoh <gms245@naver.com>
2024-04-15 01:25:20 +0900

    add

--- NativeApp/front/src/screens/auth/LoginScreen.tsx
+++ NativeApp/front/src/screens/auth/LoginScreen.tsx
@@ -1,67 +1,55 @@
 import {SafeAreaView, StyleSheet, View} from 'react-native';
 import React, {useState} from 'react';
 import InputField from '../../components/InputField';
+import CustomButton from '../../components/CustomButton';
+import useForm from '../../hooks/useForm';
+import {validateLogin} from '../../utils';
 
 // inputField에서 inputMode를 사용해 사용자의 키보드를 제어가능
 // inputMode="numeric" 사용 시 숫자키만 나오게 할 수 있음
 export default function LoginScreen() {
-  // const [email, setEmail] = useState('');
-  // const [password, setPassWord] = useState('');
-
-  // const handleChangeEmail = (text: string) => {
-  //   setEmail(text);
-  // };
-
-  // const handleChangePassword = (text: string) => {
-  //   setPassWord(text);
-  // };
-
-  const [values, setValues] = useState({
-    email: '',
-    password: '',
-  });
-
-  const [touched, setTouched] = useState({
-    email: false,
-    password: false,
-  });
-
-  const handleChangeText = (name: string, text: string) => {
-    setValues({
-      ...values,
-      [name]: text,
-    });
+  const handleSubmit = () => {
+    console.log('values', login.values);
   };
 
-  const handleBlur = (name: string) => {
-    setTouched({
-      ...touched,
-      [name]: true,
-    });
-  };
+  const login = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+    },
+    validate: validateLogin,
+  });
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <InputField
           placeholder="이메일"
-          error={'이메일을 입력해주세요'}
+          error={login.errors.email}
           inputMode="email"
-          value={values.email}
-          touched={touched.email}
-          onChangeText={text => handleChangeText('email', text)}
-          onBlur={() => handleBlur('email')}
+          touched={login.touched.email}
+          // value={values.email}
+          // onChangeText={text => handleChangeText('email', text)}
+          // onBlur={() => handleBlur('email')}
+          {...login.getTextInputProps('email')}
         />
         <InputField
           placeholder="비밀번호"
-          error={'비밀번호를 입력해주세요'}
+          error={login.errors.password}
           secureTextEntry
-          value={values.password}
-          touched={touched.password}
-          onChangeText={text => handleChangeText('password', text)}
-          onBlur={() => handleBlur('password')}
+          touched={login.touched.password}
+          // value={values.password}
+          // onChangeText={text => handleChangeText('password', text)}
+          // onBlur={() => handleBlur('password')}
+          {...login.getTextInputProps('password')}
         />
       </View>
+      <CustomButton
+        label="로그인"
+        variant="filled"
+        size="large"
+        onPress={handleSubmit}
+      />
     </SafeAreaView>
   );
 }
@@ -73,5 +61,6 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     gap: 20,
+    marginBottom: 30,
   },
 });
