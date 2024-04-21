7c20019aadde56ca95283122f7fa7a8314852347
heunoh <gms245@naver.com>
2024-04-22 02:13:28 +0900

    commit

--- NativeApp/front/src/screens/map/MapHomeScreen.tsx
+++ NativeApp/front/src/screens/map/MapHomeScreen.tsx
@@ -1,15 +1,21 @@
 import {StyleSheet, Text, View, Button} from 'react-native';
 import React from 'react';
-import useAuth from '../../hooks/queries/useAuth';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default function MapHomeScreen() {
-  const {logoutMutation} = useAuth();
   return (
-    <View>
-      <Text>MapHomeScreen</Text>
-      <Button title="로그아웃" onPress={() => logoutMutation.mutate(null)} />
-    </View>
+    <MapView
+      style={styles.container}
+      provider={PROVIDER_GOOGLE}
+      showsMyLocationButton
+      showsUserLocation
+      followsUserLocation
+    />
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
